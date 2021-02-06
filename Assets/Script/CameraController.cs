9f800c84d10770fd72d303ae6c2f6f2bbae3754f
Zainul Ariffin <zainulcain@200.red-88-2-12.staticip.rima-tde.net>
2021-02-06 15:40:51 +0800

    travel 3d app

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+   public Transform target;
+
+   public Vector3 offset;
+
+   void Start(){
+       offset = target.position - transform.position;
+   }
+    
+    
+    void Update () {
+        transform.position = target.position - offset;
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    public Vector3 moveDirection;
+    public float gravityScale;
+
+
+    
+    void Start () {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update () {
+        
+        if(controller.isGrounded){
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+            if(Input.GetButtonDown("Jump")){
+                moveDirection.y = jumpForce;
+            }
+        }
+        
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
